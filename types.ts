edd0617e604f30f0c450808e5f2b78cebbc33346
Adam Siekierski <a@siekierski.ml>
2020-11-28 16:54:29 +0100

    Initial project setup

--- types.ts
+++ types.ts
@@ -1,11 +1,7 @@
-import { DefaultCtx, SessionContext, DefaultPublicData } from "blitz"
-import { User } from "db"
+import { DefaultCtx, SessionContext } from "blitz"
 
 declare module "blitz" {
   export interface Ctx extends DefaultCtx {
     session: SessionContext
-  }
-  export interface PublicData extends DefaultPublicData {
-    userId: User["id"]
   }
 }
